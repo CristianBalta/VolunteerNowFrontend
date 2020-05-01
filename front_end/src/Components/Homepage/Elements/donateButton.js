be67910f496226ba2bc51aaaf33b4ad7671907af
Arina Badistru <arina.badistru@gmail.com>
2020-05-01 17:26:28 +0300

    c

---
+++ front_end/src/Components/Homepage/Elements/donateButton.js
@@ -0,0 +1,82 @@
+import React from "react";
+import Popover from "@material-ui/core/Popover";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import MonetizationOnIcon from "@material-ui/icons/MonetizationOn";
+
+const useStyles = makeStyles((theme) => ({
+  popover: {
+    pointerEvents: "none",
+  },
+  paper: {
+    padding: theme.spacing(1),
+  },
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    flexWrap: "nowrap",
+    transform: "translateZ(0)",
+  },
+  titleBar: {
+    background:
+      "linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
+  },
+  icon: {
+    color: "rgba(255, 255, 255, 0.54)",
+  },
+}));
+
+export default function MouseOverPopover() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+
+  return (
+    <div>
+      <IconButton aria-label="Donate" style={{ color: "#2A7549" }}>
+        <MonetizationOnIcon
+          aria-owns={open ? "mouse-over-popover" : undefined}
+          aria-haspopup="true"
+          onMouseEnter={handlePopoverOpen}
+          onMouseLeave={handlePopoverClose}
+        />
+      </IconButton>
+      <Popover
+        id="mouse-over-popover"
+        className={classes.popover}
+        classes={{
+          paper: classes.paper,
+        }}
+        open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+        onClose={handlePopoverClose}
+        disableRestoreFocus
+      >
+        <Typography>Donate</Typography>
+      </Popover>
+    </div>
+  );
+}
--- front_end/src/Components/Homepage/Homepage.jsx
+++ front_end/src/Components/Homepage/Homepage.jsx
@@ -2,16 +2,22 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import "./Homepage.css";
 import Typography from "@material-ui/core/Typography";
-import IconButton from '@material-ui/core/IconButton';
-import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
-import Popover from '@material-ui/core/Popover';
-import { stylesHomepage } from "./HomePageStyle";
-import { withStyles } from "@material-ui/core/styles";
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import { tileData } from './HomePageStyle';
-import InfoIcon from '@material-ui/icons/Info';
+import Link from "@material-ui/core/Link";
+import MouseOverPopover from "./Elements/donateButton";
+import SingleLineGridList from "./Elements/gridList";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
 class Homepage extends React.Component {
   constructor(props) {
@@ -21,64 +27,67 @@ class Homepage extends React.Component {
       title: " ",
       open: false,
       anchorEl: {},
-      option: ""
-    }
+      option: "",
+    };
   }
 
   handlePopoverClose = () => {
     this.setState({
       open: false,
-      anchorEl: null
+      anchorEl: null,
     });
-  }
+  };
 
   handlePopoverOpen = (event) => {
     this.setState({
       open: true,
-      anchorEl: event.currentTarget
+      anchorEl: event.currentTarget,
     });
-  }
+  };
   handlePopoverClose1 = () => {
     this.setState({
       open: false,
       anchorEl: null,
-      option: ""
+      option: "",
     });
-  }
+  };
 
   handlePopoverOpen1 = (event, optiune) => {
-
     this.setState({
       open: true,
       anchorEl: event.currentTarget,
-      option: optiune
+      option: optiune,
     });
-  }
+  };
 
   render() {
-    const { classes } = this.props;
-    const { classes2 } = this.props;
+    // const { classes } = this.props;
+
     return (
       <React.Fragment>
-        <div class="main">
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="//fonts.googleapis.com/css?family=Six+Caps"
-          />
-          <div class="title">
-            <Typography component="h1" variant="h5">
-              <h1>VOLUNTEER NOW</h1>
-            </Typography>
-          </div>
+        <div>
+          <div class="main">
+            <MouseOverPopover />
+            <link
+              rel="stylesheet"
+              type="text/css"
+              href="//fonts.googleapis.com/css?family=Six+Caps"
+            />
+            <div class="title">
+              <Typography component="h1" variant="h5">
+                <h1>VOLUNTEER NOW</h1>
+              </Typography>
+            </div>
 
-          <div class="subtitle">
-            <Typography
-              style={{
-                color: "white"
-              }}>
-              <h2>helping hands, caring hearts</h2>
-            </Typography>
+            <div class="subtitle">
+              <Typography
+                style={{
+                  color: "white",
+                }}
+              >
+                <h2>helping hands, caring hearts</h2>
+              </Typography>
+            </div>
           </div>
 
           <div class="buttonDiv">
@@ -90,119 +99,42 @@ class Homepage extends React.Component {
                 color: "#a0b7c6",
                 fontFamily: "Helvetica",
                 textTransform: "capitalize",
-                backgroundColor: "#e8e2da"
+                backgroundColor: "#e8e2da",
               }}
-
               variant="contained"
             >
               Join Now
-          </Button>
+            </Button>
           </div>
-
-          <IconButton aria-label="Donate" style={{ color: "#2A7549" }}
-            onMouseEnter={this.handlePopoverOpen}
-            onMouseLeave={this.handlePopoverClose}>
-            <Popover
-              id="mouse-over-popover"
-              className={classes.popover}
-              classes={{
-                paper: classes.paper,
-              }}
-              open={this.state.open}
-              anchorEl={this.state.anchorEl}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              onClose={this.handlePopoverClose}
-              disableRestoreFocus
-            >
-              <Typography>Donate</Typography>
-            </Popover>
-            <MonetizationOnIcon />
-          </IconButton>
-
         </div>
 
-        <div>
-
-        <GridList className={classes2.gridList} cellHeight='auto' cols='3' spacing='40'>
-          {tileData.map((tile) => (
-            <GridListTile key={tile.title}>
-              <img src={tile.img} />
-
-              <GridListTileBar
-                classes={{
-                  root: classes2.titleBar,
-                  title: classes2.title,
-                }
-                }
-                actionIcon={
-                  <IconButton aria-label={`info about ${tile.title}`} className={classes2.icon}
-                    onClick={(event) => this.handlePopoverOpen1(event, tile.title)}
-                  >
-                    <InfoIcon />
-                  </IconButton>
-                }
-              />
-
-            </GridListTile>
-
-          ))}
-
-        </GridList>
-
-        <Popover
-
-          className={classes2.popover}
-          classes2={{
-            paper: classes2.paper,
-          }}
-          open={this.state.open}
-          anchorEl={this.state.anchorEl}
-          anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'left',
-          }}
-          transformOrigin={{
-            vertical: 'bottom',
-            horizontal: 'right',
-          }}
-
-          onClose={this.handlePopoverClose1}
-          disableRestoreFocus
-        >
-          {this.state.option === "need" ? <React.Fragment>
-            <Typography> Financial issues </Typography>
-            <Typography> Running errands </Typography>
-            <Typography> Shopping for food/med supplies </Typography>
-            <Typography> Pet support </Typography>
-          </React.Fragment> : this.state.option === "donor" ? <React.Fragment>
-            <Typography>  Providing med supplies </Typography>
-            <Typography> Providing food </Typography>
-            <Typography> Bills and rent payment </Typography>
-            <Typography> Small loans </Typography>
-          </React.Fragment> : this.state.option === "volunteer" ? <React.Fragment>
-            <Typography>  Running errands </Typography>
-            <Typography> Delivery services </Typography>
-            <Typography> Pet support </Typography>
-          </React.Fragment> :
-                <React.Fragment>
-                </React.Fragment>
-          }
-
-        </Popover>
-
+        <div class="space">
+          <br></br>
         </div>
-
-        </React.Fragment>
-        
+        <SingleLineGridList />
+        <div class="space">
+          <br></br>
+        </div>
+        <footer class="Footer">
+          <div class="space">
+            <br></br>
+          </div>
+          <Typography variant="h6" align="center" gutterBottom>
+            Footer
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            align="center"
+            color="textSecondary"
+            component="p"
+          >
+            Something here to give the footer a purpose!
+          </Typography>
+          <Copyright/>
+        </footer>
+      </React.Fragment>
     );
   }
 }
 
-export default withStyles(stylesHomepage)(Homepage);
+export default Homepage;
