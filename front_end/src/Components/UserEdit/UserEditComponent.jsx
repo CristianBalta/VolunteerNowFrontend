ef34d50e67a3d5df7320bd869b3a9812a0c8f54c
mihneamihailescu <mihailescumihnea@gmail.com>
2020-05-01 11:35:17 +0300

    Added frontend part for user edit

--- front_end/src/Components/UserEdit/UserEditComponent.jsx
+++ front_end/src/Components/UserEdit/UserEditComponent.jsx
@@ -2,67 +2,108 @@ import React from "react"
 import axiosInstance from "../../Axios/Axios"
 import { USER_EDIT_API_ENDPOINT } from "../../Utils/utils"
 import { DONATIONS_API_ENDPOINT } from "../../Utils/utils"
+import { USER_DATA_API_ENDPOINT } from "../../Utils/utils"
 import DonationCardDummy from "../Donations/DonationCard/DonationCardDummy"
 
 import { Typography, Divider, TextField, Button } from "@material-ui/core"
 
-let userId = "";
+let userId = "5ea1d2c5b53ebd1010fe7962";
 let Lastname = "";
 let Firstname = "";
 let Email = "";
 let Telephone = "";
-let Adress = "";
-let Type = "";
-let Password = "";
+let Address = "";
+
 
 class UserEditComponent extends React.Component {
-    constructor(props) {
-                super(props)
-                this.state = {
-                    user: [{
-                        "title": "default"
-                    }]
-                }
-            }
+     constructor(props) {
+        super(props)
+        this.state = {
+            user: [{
+                "firstname" : "default",
+                "lastname" : "default",
+                "email" : "default",
+                "telephone" : "default",
+                "address"  : "default"
+            }]
+        }
+        
+        this.DisplayUserData();
+    }
 
-                /**POST */
+                /**POST */ // Send the updated data 
     UpdateUser = () => {
-        axiosInstance.post(USER_EDIT_API_ENDPOINT, { "_id": userId, "Lastname": Lastname, "Firstname": Firstname, "Email" : Email, "Telephone" : Telephone, "Adress" : Adress, "Type" :Type }).then(() => {
-           // this.DisplayUserData();
+      
+        axiosInstance.put(USER_EDIT_API_ENDPOINT + '/' + userId, {"Lastname": this.state.user.lastname, "Firstname": this.state.user.firstname, "Email" : this.state.user.email, "Telephone" : this.state.user.telephone, "Address" : this.state.user.address}).then(() => {
         })
     }
 
     /**GET */
     DisplayUserData = () => {
-        axiosInstance.get(USER_EDIT_API_ENDPOINT).then(response => {
+        axiosInstance.get(USER_DATA_API_ENDPOINT + '/' + userId).then(response => {
             this.setState({
                 user: response.data
             })
+          
         });
     }
-
+  
     setLastname = (event) => {
         Lastname = event.target.value;
+        this.setState({user : {
+            firstname : Firstname,
+            lastname : event.target.value,
+            email : Email,
+            telephone : Telephone,
+            address  : Address,
+        }});
     }
 
     setFirstname = (event) => {
+      
         Firstname = event.target.value;
+        this.setState({user : {
+                firstname : event.target.value,
+                lastname : Lastname,
+                email : Email,
+                telephone : Telephone,
+                address  : Address,
+            }});
+        
     }
 
     setEmail = (event) => {
         Email = event.target.value;
+        // this.setState({user : {email : event.target.value}});
+        this.setState({user : {
+            firstname : Firstname,
+            lastname : Lastname,
+            email : event.target.value,
+            telephone : Telephone,
+            address  : Address,
+        }});
     }
 
     setTelephone = (event) => {
         Telephone = event.target.value;
+        this.setState({user : {
+            firstname : event.target.value,
+            lastname : Lastname,
+            email : Email,
+            telephone : event.target.value,
+            address  : Address,
+        }});
     }
 
-    setAdress = (event) => {
-        Adress = event.target.value;
-    }
-
-    setType = (event) => {
-        Type = event.target.value;
+    setAddress = (event) => {
+        Address = event.target.value;
+        this.setState({user : {
+            firstname : event.target.value,
+            lastname : Lastname,
+            email : Email,
+            telephone : Telephone,
+            address  : event.target.value,
+        }});
     }
 
     render() {
@@ -70,122 +111,20 @@ class UserEditComponent extends React.Component {
                 <React.Fragment>
     
                     <Typography variant="h4">Edit User</Typography>
-                    <Divider></Divider>
-                    {/* <DonationCardDummy cards={this.state.donations} changeSum={this.changeSum} deleteCard={this.deleteCard}></DonationCardDummy> */}
-    
+                    <Divider></Divider>               
                     <br></br><br></br>
-    
-                
+              
                     <Divider></Divider>
-                    <TextField id="outlined-basic" label="Firstname" variant="outlined" onChange={this.setFirstname} />
-                    <TextField id="outlined-basic" label="Lastname" variant="outlined" onChange={this.setLastname} />
-                    <TextField id="outlined-basic" label="Email" variant="outlined" onChange={this.setEmail} />
-                    <TextField id="outlined-basic" label="Telephone" variant="outlined" onChange={this.setTelephone} />
-                    <TextField id="outlined-basic" label="Adress" variant="outlined" onChange={this.setAdress} />
-                    <TextField id="outlined-basic" label="Type" variant="outlined" onChange={this.setType} />
-                
-                    <Button onClick={this.UpdateUser}>Submit changes</Button>
-    
-    
+                               
+                    <TextField id="outlined-basic" label="Firstname" variant="outlined" value={this.state.user.firstname} onChange={this.setFirstname} />
+                    <TextField id="outlined-basic" label="Lastname" variant="outlined" value={this.state.user.lastname} onChange={this.setLastname} />
+                    <TextField id="outlined-basic" label="Email" variant="outlined" value={this.state.user.email} onChange={this.setEmail} />
+                    <TextField id="outlined-basic" label="Telephone" variant="outlined" value={this.state.user.telephone} onChange={this.setTelephone} />
+                    <TextField id="outlined-basic" label="Adress" variant="outlined" value={this.state.user.address} onChange={this.setAddress} />
+                   
+                    <Button onClick={this.UpdateUser}>Submit changes</Button>    
                 </React.Fragment>
             )
         }
 }
 export default UserEditComponent
-
-
-// let title = "";
-// let description = "";
-// let sum = 0;
-
-// class DonationComponent extends React.Component {
-
-//     constructor(props) {
-//         super(props)
-//         this.state = {
-//             donations: [{
-//                 "title": "default"
-//             }]
-//         }
-//     }
-
-
-//     componentDidMount() {
-//         this.refreshCards()
-//     }
-
-//     /**PUT */
-//     changeSum = (card, sum) => {
-
-//         card.sum += sum;
-//         axiosInstance.put(DONATIONS_API_ENDPOINT + "/" + card.id, card).then(() => {
-//             this.refreshCards()
-//         });
-
-//     }
-
-//     /**DELETE */
-//     deleteCard = (cardid) => {
-//         axiosInstance.delete(DONATIONS_API_ENDPOINT + "/" + cardid).then(() => {
-//             this.refreshCards()
-//         });
-//     }
-
-//     /**POST */
-//     createCard = () => {
-//         axiosInstance.post(DONATIONS_API_ENDPOINT, { "Title": title, "Description": description, "Sum": parseInt(sum) }).then(() => {
-//             this.refreshCards()
-//         })
-//     }
-
-//     /**GET */
-//     refreshCards = () => {
-//         axiosInstance.get(DONATIONS_API_ENDPOINT).then(response => {
-//             this.setState({
-//                 donations: response.data
-//             })
-//         });
-//     }
-
-//     /** HELPER FUNCTIONS */
-//     getTitle = (event) => {
-//         title = event.target.value;
-//     }
-
-//     getDescription = (event) => {
-//         description = event.target.value;
-//     }
-
-//     getSum = (event) => {
-//         sum = event.target.value;
-//     }
-
-
-//     render() {
-//         return (
-//             <React.Fragment>
-
-//                 <Typography variant="h4">Get all donations</Typography>
-//                 <Divider></Divider>
-//                 <DonationCardDummy cards={this.state.donations} changeSum={this.changeSum} deleteCard={this.deleteCard}></DonationCardDummy>
-
-//                 <br></br><br></br>
-
-//                 <Typography variant="h4">Create donation</Typography>
-//                 <Divider></Divider>
-//                 <TextField id="outlined-basic" label="Title" variant="outlined" onChange={this.getTitle} />
-//                 <TextField id="outlined-basic" label="Description" variant="outlined" onChange={this.getDescription} />
-//                 <TextField id="outlined-basic" label="Sum" variant="outlined" onChange={this.getSum} />
-//                 <Button onClick={this.createCard}>Create Donation</Button>
-
-
-//             </React.Fragment>
-//         )
-//     }
-
-// }
-
-// export default DonationComponent
-
-
-
--- front_end/src/Utils/utils.js
+++ front_end/src/Utils/utils.js
@@ -2,5 +2,6 @@
 export const LOCALHOST_ADDRESS_LUCI =  "https://localhost:44375/"
 
 export const DONATIONS_API_ENDPOINT = "api/donations"
-export const USER_EDIT_API_ENDPOINT = "api/userEdit"
 export const REGISTER_API_ENDPOINT = "api/users"
+export const USER_EDIT_API_ENDPOINT = "api/users/updateUser"
+export const USER_DATA_API_ENDPOINT = "api/users/getUser"
