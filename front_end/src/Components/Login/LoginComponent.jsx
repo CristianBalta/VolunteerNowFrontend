2025510c0db3bdc24d5c2b1176fe3c153e6b1f0c
Arina Badistru <arina.badistru@gmail.com>
2020-05-05 17:05:18 +0300

    login

--- front_end/src/Components/Login/LoginComponent.jsx
+++ front_end/src/Components/Login/LoginComponent.jsx
@@ -12,151 +12,156 @@ import {
   Avatar,
 } from "@material-ui/core";
 import { loginStyles } from "./LoginStyles";
+import { divStyle } from "./LoginStyles";
 import { Redirect } from "react-router-dom";
-import base64 from 'react-native-base64'
-
+import base64 from "react-native-base64";
+import "./LoginCSS.css";
+import Logo from "../../Images/logo2.png";
 let email = "";
 let password = "";
 
 class LoginComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: null,
+      checker: false,
+    };
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            redirect: null,
-            checker: false 
-        }
-    }
-
-    login = () => {
-        if ((email !== "") && (password !== "")) {
-            axiosInstance.post(LOGIN_API_ENDPOINT, {
-                "Email": email, "Password": btoa(password)
-            }).then(response => {
-                this.setState({
-                    user: JSON.parse(base64.decode(response.data))
-                    
-                })
-                if (this.state.user["Error"] === "user not found") {
-                    alert("User does not exist!");
-                    this.setChecker();
-                }
-
-                else if (this.state.user["Error"] === "wrong password")
-                    alert("Wrong password!");
-                
-                else {                   
-                    const user = this.state.user.Id + " " + this.state.user.Type;
-                    alert("User " + user + " logged in successfully");
-                    localStorage.setItem("authToken", this.state.user.Id);
-                    localStorage.setItem("userType", this.state.user.Type);
-                    this.setState({ redirect: "/dashboard" });
-                }
-
-            }).catch((error) => {
-
-                alert(error);
-
-            });
-        }
-    }
-
-    setChecker = () => {
-        this.setState({
-            checker: true
+  login = () => {
+    if (email !== "" && password !== "") {
+      axiosInstance
+        .post(LOGIN_API_ENDPOINT, {
+          Email: email,
+          Password: btoa(password),
         })
         .then((response) => {
-          if (response.data == "user not found") {
+          this.setState({
+            user: JSON.parse(base64.decode(response.data)),
+          });
+          if (this.state.user["Error"] === "user not found") {
             alert("User does not exist!");
-            this.setRedirect();
-          } else if (response.data == "wrong password")
+            this.setChecker();
+          } else if (this.state.user["Error"] === "wrong password")
             alert("Wrong password!");
-          else alert("User " + response.data + " logged in successfully");
+          else {
+            const user = this.state.user.Id + " " + this.state.user.Type;
+            alert("User " + user + " logged in successfully");
+            localStorage.setItem("authToken", this.state.user.Id);
+            localStorage.setItem("userType", this.state.user.Type);
+            this.setState({ redirect: "/dashboard" });
+          }
         })
         .catch((error) => {
           alert(error);
         });
     }
+  };
 
-    getEmail = (event) => {
-        email = event.target.value;
+  setChecker = () => {
+    this.setState({
+      checker: true,
+    })
+      .then((response) => {
+        if (response.data == "user not found") {
+          alert("User does not exist!");
+          this.setRedirect();
+        } else if (response.data == "wrong password") alert("Wrong password!");
+        else alert("User " + response.data + " logged in successfully");
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
+
+  getEmail = (event) => {
+    email = event.target.value;
+  };
+
+  getPassword = (event) => {
+    password = event.target.value;
+  };
+
+  render() {
+    const { classes } = this.props;
+    if (this.state.redirect) {
+      return <Redirect to={this.state.redirect} />;
     }
-
-    getPassword = (event) => {
-        password = event.target.value;
-    }
-
-    render() {
-        const { classes } = this.props;
-        if (this.state.redirect) {
-            return <Redirect to={this.state.redirect} />
-        }
-        return (
-            <Container component="main" maxWidth="xs">
-                <div className={classes.paper}>
-
-                    {//TODO add here volunteer logo? 
-                    }
-                    <Avatar className={classes.avatar}>
+    return (
+      <div className="main1">
+        <Container component="main" maxWidth="xs">
+          <br></br>
+          <div className={classes.paper} style={divStyle}>
+            {
+              //TODO add here volunteer logo?
+            }
+            {/* <Avatar className={classes.avatar}>
                         VN
                     </Avatar>
+                    <img src = "../../Images/logo.png"/> */}
+            <Avatar
+              alt="Remy Sharp"
+              src={Logo}
+              className={classes.large}
+            />
 
-                    <Typography component="h1" variant="h5">
-                        Sign in
-                    </Typography>
-                    <div className={classes.form} noValidate>
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                            onChange={this.getEmail}
-                        />
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                            onChange={this.getPassword}
-                        />
-                        <Button
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            className={classes.submit}
-                            onClick={this.login}
-                        >
-                            Sign In
-                             </Button>
-                        <Grid container>
-                            <Grid>
-                                {
-                                    this.state.checker ? (
-                                        <div>
-                                            <Link href="/register">
-                                                <Typography color="secondary">Don't have an account yet?</Typography>
-                                            </Link>
-                                        </div>
-                                    ) : null
-                                }
-                            </Grid>
-                        </Grid>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <div className={classes.form} noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                onChange={this.getEmail}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={this.getPassword}
+              />
+              <Button
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+                onClick={this.login}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid>
+                  {this.state.checker ? (
+                    <div>
+                      <Link href="/register">
+                        <Typography color="secondary">
+                          Don't have an account yet?
+                        </Typography>
+                      </Link>
                     </div>
-                </div>
-            </Container>
-        )
-    }
-
+                  ) : null}
+                </Grid>
+              </Grid>
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default withStyles(loginStyles)(LoginComponent);
--- front_end/src/Components/Login/LoginStyles.js
+++ front_end/src/Components/Login/LoginStyles.js
@@ -1,25 +1,46 @@
-export const loginStyles = theme => ({
-    paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: "#f5767d",
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-        backgroundColor : "#6291b0"
-    },
-    notchedOutline: {
-       
-        borderColor: "#6291b0 !important"
-      },
-  
+import Background from "../../Images/background.png"
+
+export const loginStyles = (theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: "#f5767d",
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: "#6291b0",
+  },
+  notchedOutline: {
+    borderColor: "#6291b0 !important",
+  },
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "#eaeaea"
+  },
+  large: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
 });
+
+export const divStyle = {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "white",
+    borderRadius: "10px",
+    padding: "50px",
+    
+    
+}
+
