4a262d8cf4a22c4e289607543a15709971d58b45
Andrei <Muiedinamo97>
2020-05-06 18:08:43 +0300

    need delete&update + modala

--- front_end/src/Components/Needs/NeedsCard.jsx
+++ front_end/src/Components/Needs/NeedsCard.jsx
@@ -5,8 +5,7 @@ import CardActions from '@material-ui/core/CardActions';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
-
-const NeedsCardNevoias = props => {
+const NeedsCard = props => {
     return props.cards.map(card => (
         <React.Fragment>
             <br></br>
@@ -22,17 +21,29 @@ const NeedsCardNevoias = props => {
                         {card.description}
                     </Typography>
 
+                    <Typography color="textPrimary">
+                        {card.date}
+                    </Typography>
+
                 </CardContent>
 
             </Card>
 
-            <CardActions>
+            { localStorage.getItem("userType") === "volunteer" ?
+                <CardActions>
 
-                <Button size="small" onClick={() => props.updateCard(card.id)}>Update</Button>
-                <Button size="small" onClick={() => props.saveCard(card.id)}>Save</Button>
-                <Button size="small" onClick={() => props.deleteCard(card.id)}>Delete</Button>
+                    <Button size="small" onClick={() => props.assignCard(card.id)}>Assign</Button>
 
-            </CardActions>
+                </CardActions>
+            :
+                <CardActions>
+
+                    <Button size="small" onClick={() => props.updateCard(card.id)}>Update</Button>
+                    <Button size="small" onClick={() => props.deleteCard(card.id)}>Delete</Button>
+
+                </CardActions>
+            }
+            
         </React.Fragment>
     )
     )
@@ -40,4 +51,4 @@ const NeedsCardNevoias = props => {
 };
 
 
-export default NeedsCardNevoias;
+export default NeedsCard;
